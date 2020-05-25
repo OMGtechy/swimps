40759f31946130947f2af6095276a8f97b6a2f7b
Joshua Gerrard <joshuagerrard@protonmail.com>
2020-05-25 14:50:47 +0100

    Add error message for when fork fails

--- swimps-profile/source/swimps-profile.c
+++ swimps-profile/source/swimps-profile.c
@@ -1,21 +1,42 @@
 #include "swimps-profile.h"
+#include "swimps-log.h"
+#include "swimps-io.h"
 
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
 swimps_error_code_t swimps_profile(const char* const executable) {
     const pid_t pid = fork();
 
     switch(pid) {
-    // Fork failed.
-    case -1:
-        // TODO: log message.
+    case -1: {
+        // Fork failed.
+        char logMessageBuffer[128] = { 0 };
+        const char* const formatString = "fork failed, errno %d.";
+
+        const size_t bytesWritten = swimps_format_string(formatString,
+                                                         strlen(formatString),
+                                                         logMessageBuffer,
+                                                         sizeof logMessageBuffer,
+                                                         errno);
+
+        char formattedLogMessageBuffer[128] = { 0 };
+        swimps_format_log_message(SWIMPS_LOG_LEVEL_DEBUG,
+                                  logMessageBuffer,
+                                  bytesWritten,
+                                  formattedLogMessageBuffer,
+                                  sizeof formattedLogMessageBuffer);
+
         return SWIMPS_ERROR_FORK_FAILED;
-    // We're the child process.
+    }
     case 0:
+        // We're the child process.
         // TODO: implement child process.
+        (void) executable;
         return SWIMPS_ERROR_NONE;
-    // We're the parent process.
     default:
+        // We're the parent process.
         return SWIMPS_ERROR_NONE;
     }
 }
