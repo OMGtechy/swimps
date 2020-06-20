be561ba6850c1499b86e5f59f39e2271bc7dcf00
Joshua Gerrard <joshuagerrard@protonmail.com>
2020-06-20 16:57:27 +0100

    Generate a trace file when swimps-preload is loaded.

--- swimps-preload/source/swimps-preload.c
+++ swimps-preload/source/swimps-preload.c
@@ -1,6 +1,72 @@
-#include "stdio.h"
+#include "swimps-time.h"
+#include "swimps-log.h"
+#include "swimps-trace-file.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// Should be set by glibc
+extern const char* program_invocation_short_name;
+
+static const clockid_t swimps_preload_clock_id = CLOCK_MONOTONIC_COARSE;
+
+static int swimps_preload_create_trace_file() {
+    swimps_timespec_t time;
+    if (swimps_gettime(swimps_preload_clock_id, &time) == -1) {
+
+        const char message[] = "Could not get time to generate trace file name.";
+        swimps_write_to_log(
+            SWIMPS_LOG_LEVEL_FATAL,
+            message,
+            sizeof message
+        );
+
+        abort();
+    }
+
+    char traceFileNameBuffer[2048] = { 0 };
+
+    const size_t bytesWritten = swimps_trace_file_generate_name(
+        program_invocation_short_name,
+        &time,
+        getpid(),
+        traceFileNameBuffer,
+        sizeof traceFileNameBuffer
+    );
+
+    // Whilst it could be *exactly* the right size,
+    // changes are there's just not enough room.
+    if (bytesWritten == sizeof traceFileNameBuffer) {
+
+        const char message[] = "Could not generate trace file name.";
+        swimps_write_to_log(
+            SWIMPS_LOG_LEVEL_FATAL,
+            message,
+            sizeof message
+        );
+
+        abort();
+    }
+
+    const int file = swimps_trace_file_create(traceFileNameBuffer);
+    if (file == -1) {
+
+        const char message[] = "Could not create trace file.";
+        swimps_write_to_log(
+            SWIMPS_LOG_LEVEL_FATAL,
+            message,
+            sizeof message
+        );
+
+        abort();
+    }
+
+    return file;
+}
 
 __attribute__((constructor))
 void swimps_preload_constructor() {
-    
+    const int traceFileDescriptor = swimps_preload_create_trace_file();
+    (void) traceFileDescriptor;
 }
---
+++ swimps-trace-file/include/swimps-trace-file.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "swimps-time.h"
+
+#include <stddef.h>
+#include <unistd.h>
+
+//!
+//! \brief  Creates a file with the necessary headers to store a swimps_trace.
+//!
+//! \param[in]  path  Where to create the file.
+//!
+//! \returns  The file descriptor for the trace file, or -1 if something went wrong.
+//!
+int swimps_trace_file_create(const char* const path);
+
+//!
+//! \brief  Generates a name for the trace file based upon a program name.
+//!
+//! \param[in]   programName       The name of the program the trace file is for.
+//! \param[in]   time              The timestamp to use when generating the name.
+//! \param[in]   pid               The process ID of the program the trace file is for.
+//! \param[out]  targetBuffer      Where to write the generated name.
+//! \param[in]   targetBufferSize  How large the target buffer is.
+//!
+//! \returns  The number of bytes written to the target buffer.
+//!
+//! \note  This function is *not* async signal safe.
+//!        TODO: should it be? Would need to implement format specifiers for int64 for that...
+//!
+size_t swimps_trace_file_generate_name(const char* const programName,
+                                       const swimps_timespec_t* const time,
+                                       const pid_t pid,
+                                       char* const targetBuffer,
+                                       const size_t targetBufferSize);
+
