dac94f13bb946521847275059a096804899312f2
Joshua Gerrard <joshuagerrard@protonmail.com>
2020-07-02 21:51:40 +0100

    Added swimps_create_signal_timer.

--- swimps-time/include/swimps-time.h
+++ swimps-time/include/swimps-time.h
@@ -31,3 +31,18 @@ typedef struct swimps_timespec {
 //! \note  This function is async signal safe.
 //!
 int swimps_gettime(const clockid_t clockID, swimps_timespec_t* const out);
+
+//!
+//! \brief  A shorthand for creating a timer that fires a signal.
+//!
+//! \param[in]   clockID  The clock you want the timer to use.
+//! \param[in]   signal   The signal you want to fire when triggered.
+//! \param[out]  out      The resulting timer.
+//!
+//! \returns  0 on success, -1 on error (errno may be set accordingly).
+//!
+//! \note  It does not start the timer.
+//!
+//! \note  This function is *not* async signal safe.
+//!
+int swimps_create_signal_timer(const clockid_t clockID, const int signal, timer_t* const out);
--- swimps-time/source/swimps-time.c
+++ swimps-time/source/swimps-time.c
@@ -1,6 +1,8 @@
 #include "swimps-time.h"
 #include "swimps-assert.h"
 
+#include <signal.h>
+
 int swimps_gettime(const clockid_t clockID, swimps_timespec_t* const out) {
 
     swimps_assert(out != NULL);
@@ -12,4 +14,15 @@ int swimps_gettime(const clockid_t clockID, swimps_timespec_t* const out) {
     out->nanoseconds = timespecNow.tv_nsec;
 
     return returnValue;
+}
+
+int swimps_create_signal_timer(const clockid_t clockID, const int signal, timer_t* const out) {
+
+    swimps_assert(out != NULL);
+
+    struct sigevent signalEvent;
+    signalEvent.sigev_notify = SIGEV_SIGNAL;
+    signalEvent.sigev_signo = signal;
+
+    return timer_create(clockID, &signalEvent, out);
 }
