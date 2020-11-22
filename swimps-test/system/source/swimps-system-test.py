326746c76c6483117cb18f6eda37595b7469a3bf
Joshua Gerrard <joshuagerrard@protonmail.com>
2020-11-22 11:46:10 +0000

    Add systems test for invalid command line values.

--- swimps-test/system/source/swimps-system-test.py
+++ swimps-test/system/source/swimps-system-test.py
@@ -6,14 +6,20 @@ import glob
 import os
 
 def main():
+    expect_zero_exit_code = sys.argv[1] == "zero"
     get_traces = lambda : glob.glob("swimps_trace_swimps-dummy*")
 
     for trace in get_traces():
         os.remove(trace)
 
-    completed_process = subprocess.run(sys.argv[1:])
-    if completed_process.returncode != 0:
-        return completed_process.returncode
+    completed_process = subprocess.run(sys.argv[2:])
+    if expect_zero_exit_code:
+        if completed_process.returncode != 0:
+            return completed_process.returncode
+    elif completed_process.returncode == 0:
+        return -1
+    else:
+        return 0
 
     traces = get_traces()
     if len(traces) != 1:
