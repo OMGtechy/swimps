1fae44d64b19464a410535333b153216a06493ec
Joshua Gerrard <joshuagerrard@protonmail.com>
2020-11-22 09:37:58 +0000

    Added a systems test for program args, swimps args and no args.

--- swimps-test/system/source/swimps-dummy.cpp
+++ swimps-test/system/source/swimps-dummy.cpp
@@ -1,15 +1,17 @@
 #include <chrono>
 #include <iostream>
 
-int main() {
+int main(int argc, char** argv) {
     std::cout << "Starting test program." << std::endl;
+
+    const auto delay = argc > 1 ? atoi(argv[1]) : 3;
 
     const auto startTime = std::chrono::steady_clock::now();
     std::chrono::time_point<std::chrono::steady_clock> currentTime;
 
     do {
         currentTime = std::chrono::steady_clock::now();
-    } while((currentTime - startTime) < std::chrono::seconds(3));
+    } while((currentTime - startTime) < std::chrono::seconds(delay));
 
     std::cout << "Ending test program." << std::endl;
 }
