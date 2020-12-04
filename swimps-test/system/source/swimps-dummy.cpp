5121d86e99c4fc5a8bf4a26a0ac8cd0ae27533db
Joshua Gerrard <joshuagerrard@protonmail.com>
2020-12-04 22:54:31 +0000

    Make swimps-dummy produce more interesting backtraces.

--- swimps-test/system/source/swimps-dummy.cpp
+++ swimps-test/system/source/swimps-dummy.cpp
@@ -1,5 +1,31 @@
 #include <chrono>
 #include <iostream>
+#include <cstdint>
+#include <cmath>
+
+using result_t = int64_t;
+
+result_t computePartA(result_t result) {
+    return result << std::min(result_t(32), std::max(result_t(1), std::abs(result + 1)));
+}
+
+result_t computePartB(result_t result) {
+    return std::round(std::sqrt(std::max(result_t(1), result)));
+}
+
+result_t computePartC(result_t result) {
+    return std::log(std::max(result_t(1), result));
+}
+
+result_t compute(result_t result) {
+    for (size_t i = 0; i < 4096; ++i) {
+        result = computePartA(result);
+        result = computePartB(result);
+        result = computePartC(result);
+    }
+
+    return result;
+}
 
 int main(int argc, char** argv) {
     std::cout << "Starting test program." << std::endl;
@@ -9,9 +35,13 @@ int main(int argc, char** argv) {
     const auto startTime = std::chrono::steady_clock::now();
     std::chrono::time_point<std::chrono::steady_clock> currentTime;
 
+    int32_t result = 0;
+
     do {
+        result = compute(result);
         currentTime = std::chrono::steady_clock::now();
     } while((currentTime - startTime) < std::chrono::seconds(delay));
 
+    std::cout << "Result: " << result << std::endl;
     std::cout << "Ending test program." << std::endl;
 }
