fd31e7cb072bda69914856a67f1b5e3e0f8e040d
Joshua Gerrard <joshuagerrard@protonmail.com>
2021-01-16 18:06:23 +0000

    Convert printNode lambda into a proper function.

--- swimps-tui/source/swimps-tui.cpp
+++ swimps-tui/source/swimps-tui.cpp
@@ -1,6 +1,5 @@
 #include "swimps-tui.h"
 
-#include <functional>
 #include <optional>
 #include <limits>
 #include <map>
@@ -17,57 +16,98 @@ using swimps::trace::stack_frame_id_t;
 using swimps::trace::StackFrame;
 using swimps::trace::Trace;
 
+namespace {
+    // We could have as many lines as stack frames, worst case.
+    using line_t = stack_frame_count_t;
+
+    using expansion_state_t = std::map<const CallTreeNode*, bool>;
+    using line_mappings_t = std::map<line_t, const CallTreeNode*>;
+
+    const StackFrame* lookup_stack_frame(const Trace& trace, const stack_frame_id_t id) {
+        const auto& stackFrames = trace.stackFrames;
+        const auto iter = std::find_if(stackFrames.cbegin(), stackFrames.cend(), [id](const auto& stackFrame) { return stackFrame.id == id; });
+        return iter != stackFrames.cend() ? &(*iter) : nullptr;
+    };
+
+    void print_node(WINDOW* const window,
+                    const Trace& trace,
+                    const CallTreeNode& rootNode,
+                    expansion_state_t& expansionState,
+                    line_mappings_t& lineMappings,
+                    const line_t selectedLine,
+                    const line_t linesToSkip,
+                    line_t& currentLine,
+                    const std::size_t indentation) {
+
+        if (currentLine >= linesToSkip) {
+            for(std::size_t i = 0; i < indentation; ++i) {
+                wprintw(window, "-");
+            }
+
+            const auto* const stackFrame = lookup_stack_frame(trace, rootNode.stackFrameID);
+            wprintw(
+                window,
+                "%c %s %s\n",
+                selectedLine == currentLine ? '>' : ' ',
+                rootNode.children.size() == 0 ? "   " : expansionState[&rootNode] ? "[-]" : "[+]",
+                stackFrame != nullptr ? stackFrame->mangledFunctionName : "?"
+            );
+        }
+
+        lineMappings[currentLine] = &rootNode;
+        currentLine += 1;
+
+        if (expansionState[&rootNode]) {
+            for(const auto& childNode : rootNode.children) {
+                print_node(
+                    window,
+                    trace,
+                    childNode,
+                    expansionState,
+                    lineMappings,
+                    selectedLine,
+                    linesToSkip,
+                    currentLine,
+                    indentation + 1
+                );
+            }
+        }
+    }
+}
+
 ErrorCode swimps::tui::run(const Trace& trace, const Analysis& analysis) {
     WINDOW* const window = initscr();
     swimps_assert(window != nullptr);
     keypad(window, true);
 
-    std::map<const CallTreeNode*, bool> expansionState;
-   
-    stack_frame_count_t selectedLine = 0;
-    stack_frame_count_t line = 0;
+    expansion_state_t expansionState;
+    line_mappings_t lineMappings;
 
-    std::map<decltype(line), const CallTreeNode*> lineMappings; 
+    line_t selectedLine = 0;
+    line_t currentLine = 0;
 
-    const auto printCallTree = [window, &line, &lineMappings, &selectedLine, &expansionState, &trace, &analysis](const stack_frame_count_t linesToSkip){
-        const auto lookupStackFrame = [&trace](const stack_frame_id_t id) -> const StackFrame* {
-            const auto& stackFrames = trace.stackFrames;
-            const auto iter = std::find_if(stackFrames.cbegin(), stackFrames.cend(), [id](const auto& stackFrame) { return stackFrame.id == id; });
-            return iter != stackFrames.cend() ? &(*iter) : nullptr;
-        };
-
-        std::function<void(const CallTreeNode&, const std::size_t)> printNode;
-        printNode = [&selectedLine, &expansionState, &line, &lineMappings, linesToSkip, &printNode, lookupStackFrame, window](const CallTreeNode& rootNode, const std::size_t indentation) {
-            if (line >= linesToSkip) {
-                for(std::size_t i = 0; i < indentation; ++i) {
-                    wprintw(window, "-");
-                }
-
-                const auto* const stackFrame = lookupStackFrame(rootNode.stackFrameID);
-                wprintw(window, "%c %s %s\n", selectedLine == line ? '>' : ' ', rootNode.children.size() == 0 ? "   " : expansionState[&rootNode] ? "[-]" : "[+]", stackFrame != nullptr ? stackFrame->mangledFunctionName : "?");
-            }
-
-            lineMappings[line] = &rootNode;
-            line += 1;
-
-            if (expansionState[&rootNode]) {
-                for(const auto& childNode : rootNode.children) {
-                   printNode(childNode, indentation + 1); 
-                }
-            }
-        };
-
+    const auto printCallTree = [window, &currentLine, &lineMappings, &selectedLine, &expansionState, &trace, &analysis](const stack_frame_count_t linesToSkip){
         for(const auto& rootNode : analysis.callTree) {
-            printNode(rootNode, 0);
+            print_node(
+                window,
+                trace,
+                rootNode,
+                expansionState,
+                lineMappings,
+                selectedLine,
+                linesToSkip,
+                currentLine,
+                0
+            );
         }
     };
 
-    stack_frame_count_t callTreeOffset = 0;
+    line_t callTreeOffset = 0;
 
     bool quit = false;
     while(!quit) {
         werase(window);
-        line = 0;
+        currentLine = 0;
         printCallTree(callTreeOffset);
         wrefresh(window);
         const int input = wgetch(window);
