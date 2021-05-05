49216762db78b5a35958a9eea1d83e6e44506d77
Joshua Gerrard <joshuagerrard@protonmail.com>
2021-05-06 00:18:22 +0100

    Add VSCode dev container config.

---
+++ .devcontainer/create-default-build-directories.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+mkdir build
+cd build
+mkdir gcc-10-debug clang-11-debug gcc-10-release clang-11-release
+cd gcc-10-debug
+CC=gcc-10 CXX=g++-10 cmake -DCMAKE_BUILD_TYPE=Debug ../../
+make -j
+make test
+cd ../gcc-10-release
+CC=gcc-10 CXX=g++-10 cmake -DCMAKE_BUILD_TYPE=Release ../../
+make -j
+make test
+cd ../clang-11-debug
+CC=clang-11 CXX=clang++-11 cmake -DCMAKE_BUILD_TYPE=Debug ../../
+make -j
+make test
+cd ../clang-11-release
+CC=clang-11 CXX=clang++-11 cmake -DCMAKE_BUILD_TYPE=Release ../../
+make -j
+make test
