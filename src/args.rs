3411f5adad271cbfed0ebeeb2e16dbbc772b1711
Artemis Meursault Gerrard <OMGtechy@users.noreply.github.com>
2022-06-02 16:16:55 +0000

    Add target program.

--- src/args.rs
+++ src/args.rs
@@ -3,12 +3,19 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 pub struct Args {
     #[clap(long)]
-    trace_file: String
+    trace_file: String,
+
+    #[clap()]
+    target_program: String
 }
 
 impl Args {
     pub fn trace_file(&self) -> &str {
         &self.trace_file
+    }
+
+    pub fn target_program(&self) -> &str {
+        &self.target_program
     }
 }
 
@@ -18,7 +25,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn trace_file() {
-        assert_eq!("hi", Args::parse_from(["test", "--trace-file", "hi"].iter()).trace_file());
+    fn trace_file_and_program() {
+        let args = Args::parse_from([
+            "test",
+            "--trace-file",
+            "hi",
+            "echo"
+        ].iter());
+    
+        assert_eq!("hi", args.trace_file());
+        assert_eq!("echo", args.target_program());
     }
 }
