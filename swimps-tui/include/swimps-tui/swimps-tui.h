a2ef000fc79580215336f9c98c499e627ef3b801
Artemis Meursault Gerrard <OMGtechy@users.noreply.github.com>
2022-06-03 11:59:47 +0000

    Remove C++ implementation.
    We have source control and can use that to refer back to it when required.

--- swimps-tui/include/swimps-tui/swimps-tui.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "swimps-analysis/swimps-analysis.h"
-#include "swimps-error/swimps-error.h"
-#include "swimps-trace/swimps-trace.h"
-
-namespace swimps::tui {
-    swimps::error::ErrorCode run(const swimps::trace::Trace&, const swimps::analysis::Analysis&);
-}
