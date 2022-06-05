a43322585f25af3f2bfb1b278d11e04a9576e629
Artemis Meursault Gerrard <OMGtechy@users.noreply.github.com>
2022-06-05 11:44:19 +0000

    Call codeinjector_inject_library with dummy args.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
 pub mod args;
 
+use std::ffi::CString;
+use std::os::raw::c_char;
+
+extern "C" {
+    fn codeinjector_inject_library(program: *const c_char, args: *const *const c_char, argsCount: usize, lib: *const c_char) -> bool;
+}
+
 fn main() {
-    let args = args::Args::parse(); 
-    println!("Trace file: {:?}", args);
+    let args = args::Args::parse();
+    let dummy_args = [
+        CString::new("a").unwrap(),
+        CString::new("b").unwrap(),
+        CString::new("c").unwrap()
+    ];
+
+    let dummy_arg_ptrs = dummy_args.map(|arg| arg.as_ptr());
+    let dummy_program = CString::new("testprog").unwrap();
+    let dummy_lib = CString::new("testlib").unwrap();
+
+    unsafe {
+        codeinjector_inject_library(
+            dummy_program.as_ptr(),
+            dummy_arg_ptrs.as_ptr(),
+            dummy_arg_ptrs.len(),
+            dummy_lib.as_ptr()
+        );
+    }
 }
