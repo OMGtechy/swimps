6db4a8c755f2892e08077a016f76b4d7f0f76a73
Artemis Meursault Gerrard <OMGtechy@users.noreply.github.com>
2022-06-25 09:59:59 +0000

    Use args::Args.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 pub mod args;
 pub mod child;
 
+use args::Args;
 use child::child;
 use fork::{fork, Fork};
 
 fn main() {
-    let args = args::Args::parse();
+    let args = Args::parse();
     match fork() {
         Ok(Fork::Parent(child_pid)) => { println!("Child process {} started.", child_pid)},
         Ok(Fork::Child) => { child(&args) },
