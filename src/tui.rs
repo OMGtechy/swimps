a2271dca6e67207c0c82771eeec7232062ee33b0
Artemis Meursault Gerrard <OMGtechy@users.noreply.github.com>
2022-06-29 20:57:33 +0000

    Add table to TUI.
    I'll probably end up using this for "top functions" - just wanted to get this commited as it's a useful working reference point.

--- src/tui.rs
+++ src/tui.rs
@@ -2,7 +2,7 @@ use crate::args::Args;
 
 use crossterm::{terminal::{enable_raw_mode, EnterAlternateScreen, disable_raw_mode, LeaveAlternateScreen}, execute, event::{self, Event::Key, KeyCode, KeyModifiers}};
 use std::time::Duration;
-use tui::{Terminal, backend::CrosstermBackend, widgets::{Block, Borders}};
+use tui::{Terminal, backend::CrosstermBackend, widgets::{Block, Borders, Row, Table, TableState}, layout::Constraint};
 
 pub fn run(args: Args) {
     enable_raw_mode().expect("Cannot enable raw mode");
@@ -12,11 +12,35 @@ pub fn run(args: Args) {
         CrosstermBackend::new(stdout)
     ).expect("Cannot create terminal");
 
+    let mut table_state = TableState::default();
+    table_state.select(Some(0));
+
     loop {
         terminal.draw(|frame| {
-            frame.render_widget(
-                Block::default().title(format!(" swimps ─ {} ─ {} ", args.target_program(), args.trace_file())).borders(Borders::ALL),
-                frame.size()
+
+            let table = Table::new(
+                vec![
+                    Row::new(vec!["a", "b", "c"]),
+                    Row::new(vec!["d", "e", "f"]),
+                    Row::new(vec!["h", "i", "j"])
+                ]
+            )
+            .header(Row::new(vec!["h1", "h2", "h3"].into_iter()))
+            .block(Block::default()
+                .title(format!(" swimps ─ {} ─ {} ", args.target_program(), args.trace_file()))
+                .borders(Borders::ALL),
+            )
+            .widths(&[
+                Constraint::Ratio(1, 3),
+                Constraint::Ratio(1, 3),
+                Constraint::Ratio(1, 3),
+            ])
+            .highlight_symbol("> ");
+
+            frame.render_stateful_widget(
+                table,
+                frame.size(),
+                &mut table_state
             );
         }).expect("Cannot draw TUI");
 
@@ -25,6 +49,9 @@ pub fn run(args: Args) {
                 match key_event.code {
                     KeyCode::Char('d') | KeyCode::Char('c') if key_event.modifiers.contains(KeyModifiers::CONTROL) => break,
                     KeyCode::Char('q') => break,
+                    KeyCode::Up => table_state.select(table_state.selected().map(|n| n.saturating_sub(1))),
+                    // TODO: will want to limit this to number of rows once real data is in
+                    KeyCode::Down => table_state.select(table_state.selected().map(|n| n.saturating_add(1))),
                     _ => (),
                 }
             }
