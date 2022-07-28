2c172812244d1ee44862e575a63b93cf885b1470
Artemis Meursault Gerrard <OMGtechy@users.noreply.github.com>
2022-07-28 09:17:36 +0000

    Remove need to samples to be mutable

--- src/trace/optimised/trace.rs
+++ src/trace/optimised/trace.rs
@@ -1,7 +1,7 @@
 use std::{collections::HashSet, hash::Hash};
 
 use crate::trace::optimised::{backtrace::{Backtrace, BacktraceID}, sample::Sample, stack_frame::{StackFrame, StackFrameID}};
-use crate::trace::raw::trace::Trace as RawTrace;
+use crate::trace::raw::{sample::Sample as RawSample, trace::Trace as RawTrace};
 use crate::trace::shared::{instruction_pointer::InstructionPointer, timestamp::Timestamp};
 
 struct StackFrameInstructionPointerEqualityWrapper(pub StackFrame);
@@ -43,9 +43,43 @@ pub struct Trace {
     pub samples: Vec<Sample>
 }
 
+fn raw_to_optimised_sample(raw_sample: &RawSample,
+                           stack_frames: &mut HashSet::<StackFrameInstructionPointerEqualityWrapper>,
+                           next_stack_frame_id: &mut StackFrameID,
+                           backtraces: &mut HashSet::<BacktraceStackFramesEqualityWrapper>,
+                           next_backtrace_id: &mut BacktraceID) -> Sample {
+    let new_backtrace = BacktraceStackFramesEqualityWrapper(
+        Backtrace {
+            id: BacktraceID(next_backtrace_id.0),
+            stack_frames: raw_sample.backtrace.0.iter().map(|ip|
+                get_or_insert_stack_frame(
+                    StackFrameInstructionPointerEqualityWrapper(StackFrame {
+                        id: StackFrameID(next_stack_frame_id.0),
+                        instruction_pointer: InstructionPointer(ip.0)
+                    }),
+                    stack_frames,
+                    next_stack_frame_id)
+            ).collect()
+        }
+    );
+
+    Sample {
+        timestamp: Timestamp { seconds: raw_sample.timestamp.seconds, nanoseconds: raw_sample.timestamp.nanoseconds },
+        backtrace: match backtraces.get(&new_backtrace) {
+            None => {
+                let id = new_backtrace.0.id.0;
+                backtraces.insert(new_backtrace);
+                *next_backtrace_id = BacktraceID(next_backtrace_id.0.checked_add(1).expect("Backtrace ID overflowed"));
+                BacktraceID(id)
+            }
+            Some(bt) => BacktraceID(bt.0.id.0)
+        }
+    }
+}
+
 fn get_or_insert_stack_frame(to_insert: StackFrameInstructionPointerEqualityWrapper,
-                                stack_frames: &mut HashSet::<StackFrameInstructionPointerEqualityWrapper>,
-                                next_stack_frame_id: &mut StackFrameID) -> StackFrameID {
+                             stack_frames: &mut HashSet::<StackFrameInstructionPointerEqualityWrapper>,
+                             next_stack_frame_id: &mut StackFrameID) -> StackFrameID {
     match stack_frames.get(&to_insert) {
         None => {
             let id = to_insert.0.id.0;
@@ -67,37 +101,14 @@ impl Trace {
         let mut backtraces = HashSet::<BacktraceStackFramesEqualityWrapper>::new();
         let mut next_backtrace_id = BacktraceID(1);
 
-        let mut samples = Vec::<Sample>::new();
-
-        for raw_sample in raw_trace.samples() {
-            let new_backtrace = BacktraceStackFramesEqualityWrapper(
-                Backtrace {
-                    id: BacktraceID(next_backtrace_id.0),
-                    stack_frames: raw_sample.backtrace.0.iter().map(|ip|
-                        get_or_insert_stack_frame(
-                            StackFrameInstructionPointerEqualityWrapper(StackFrame {
-                                id: StackFrameID(next_stack_frame_id.0),
-                                instruction_pointer: InstructionPointer(ip.0)
-                            }),
-                            &mut stack_frames,
-                            &mut next_stack_frame_id)
-                    ).collect()
-                }
-            );
-
-            samples.push(Sample {
-                timestamp: Timestamp { seconds: raw_sample.timestamp.seconds, nanoseconds: raw_sample.timestamp.nanoseconds },
-                backtrace: match backtraces.get(&new_backtrace) {
-                    None => {
-                        let id = new_backtrace.0.id.0;
-                        backtraces.insert(new_backtrace);
-                        next_backtrace_id = BacktraceID(next_backtrace_id.0.checked_add(1).expect("Backtrace ID overflowed"));
-                        BacktraceID(id)
-                    }
-                    Some(bt) => BacktraceID(bt.0.id.0)
-                }
-            });
-        }
+        let samples = raw_trace.samples().iter().map(
+            |raw_sample|
+                raw_to_optimised_sample(
+                    raw_sample,
+                    &mut stack_frames,
+                    &mut next_stack_frame_id,
+                    &mut backtraces,
+                    &mut next_backtrace_id)).collect();
 
         Trace {
             stack_frames: stack_frames.drain().map(|wrapper| wrapper.0).collect(),
